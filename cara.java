641764f431e8cc8b648e0df16151445591626247
AndreaJCharlesSantos <56329185+AndreaJCharlesSantos@users.noreply.github.com>
2021-02-22 13:29:52 -0600

    Update cara.java

--- cara.java
+++ cara.java
@@ -1,24 +1,16 @@
 import java.util.*;
 public class cara(){
-	boolean flagcenter;
-	boolean flagSide1;
-	boolean flagSide2;
-	boolean flagSide3;
-	boolean flagSide4;
-	boolean flagCorner1;
-	boolean flagCorner2;
-	boolean flagCorner3;
-	boolean flagCorner4;
 	public cara(){
-		flagcenter = false;
-		flagSide1 = false;
-		flagSide2 = false;
-		flagSide3 = false;
-		flagSide4 = false;
-		flagCorner1 = false;
-		flagCorner2 = false;
-		flagCorner3 = false;
-		flagCorner4 = false;
+		cuadrito cuadrito1 = new cuadrito();
+		cuadrito cuadrito2 = new cuadrito();
+		cuadrito cuadrito3 = new cuadrito();
+		cuadrito cuadrito4 = new cuadrito();
+		cuadrito cuadrito5 = new cuadrito();
+		cuadrito cuadrito6 = new cuadrito();
+		cuadrito cuadrito7 = new cuadrito();
+		cuadrito cuadrito8 = new cuadrito();
+		cuadrito cuadrito9 = new cuadrito();
+
 	}
 
 
