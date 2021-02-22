a49ca917e83a6e94f27b32a4c69797dd03d25751
AndreaJCharlesSantos <56329185+AndreaJCharlesSantos@users.noreply.github.com>
2021-02-22 13:14:10 -0600

    Update cara.java

--- cara.java
+++ cara.java
@@ -1,6 +1,25 @@
 import java.util.*;
 public class cara(){
-	private Color color;
-	
+	boolean flagcenter;
+	boolean flagSide1;
+	boolean flagSide2;
+	boolean flagSide3;
+	boolean flagSide4;
+	boolean flagCorner1;
+	boolean flagCorner2;
+	boolean flagCorner3;
+	boolean flagCorner4;
+	public cara(){
+		flagcenter = false;
+		flagSide1 = false;
+		flagSide2 = false;
+		flagSide3 = false;
+		flagSide4 = false;
+		flagCorner1 = false;
+		flagCorner2 = false;
+		flagCorner3 = false;
+		flagCorner4 = false;
+	}
+
 
 }
